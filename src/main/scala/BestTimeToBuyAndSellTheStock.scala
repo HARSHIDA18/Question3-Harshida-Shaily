a4939ecac7fb25d142a0735f8bfe085663b0b544
Harshida Shaily <20BCS2198@gmail.com>
2024-04-19 15:16:19 +0530

    Best time

---
+++ src/main/scala/BestTimeToBuyAndSellTheStock.scala
@@ -0,0 +1,23 @@
+import scala.annotation.tailrec
+class BestTimeToBuyAndSellTheStock extends App {
+    def maxProfit(prices: Array[Int]): Int = {
+      if (prices.isEmpty) {
+        0
+      } else {
+        require(prices.forall(_ >= 0), "Input array should contain non-negative integers")
+        @tailrec
+        def maxProfitHelp(prices: List[Int], maxProfit: Int, lastBuy: Int): Int = {
+          prices match {
+            case Nil => maxProfit
+            case head :: tail => if (head > lastBuy) {
+              maxProfitHelp(tail, Math.max(maxProfit, head - lastBuy), lastBuy)
+            } else {
+              maxProfitHelp(tail, maxProfit, head)
+            }
+          }
+        }
+        val listPrices = prices.toList
+        maxProfitHelp(listPrices.tail, 0,listPrices.head)
+      }
+    }
+}
